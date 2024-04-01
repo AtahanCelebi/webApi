e0a966dca40a03f86af4328f7288242d9c0e5a35
atahan.celebi <atahan.celebi@synchron.com.tr>
2024-04-01 16:23:45 +0300

    login method

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -9,11 +9,13 @@ namespace ProductsAPI.Controllers {
     [Route("api/[controller]")]
     public class UsersController: ControllerBase
     {
-        private UserManager<AppUser> _userManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public UsersController(UserManager<AppUser> userManager)
+        public UsersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
 
         }
 
@@ -39,5 +41,29 @@ namespace ProductsAPI.Controllers {
             }
             return BadRequest(result.Errors);   
         }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO model) {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user is null) {
+                return BadRequest(new {
+                    message = "email hatalı"
+                });
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, true);
+
+            if (result.Succeeded) {
+                return Ok(new {
+                    token = "token"
+                });
+            }
+            return Unauthorized();
+        }
+
+
+
     }
 }

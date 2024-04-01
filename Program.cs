c501723b985b2ac272b8725a4d8182240a4ca25e
atahan.celebi <atahan.celebi@synchron.com.tr>
2024-04-01 17:54:32 +0300

    jwt verification

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ProductsAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,10 +22,54 @@ builder.Services.Configure<IdentityOptions>(options => {
     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // after x times timeout
 });
 
+builder.Services.AddAuthentication(x => {
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => {
+    x.RequireHttpsMetadata = false; // false ise http den gelen istekleri de kabul eder
+    x.TokenValidationParameters = new TokenValidationParameters {
+        ValidateIssuer = false, // eğer true olursa issuer kısmında ata.com'da gönderilmeli.
+        ValidIssuer = "ata.com",
+
+        ValidateAudience = false, // eğer true olursa bu api'nin hangi servislere ve kime hitap ettiği gönderilmeli
+        ValidAudience = "a",
+
+        ValidateIssuerSigningKey = true, // burada token'ı validate edicez, 
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration.GetSection("AppSettings:Secret").Value ?? string.Empty)),  // burada ters işlem yaparak algoritmayı vericez
+        ValidateLifetime = true // eğer true olmazsa süre önemsemeden validate eder, expire olmaz
+    };
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -34,6 +82,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();

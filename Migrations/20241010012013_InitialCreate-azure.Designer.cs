0005367e78418880caed74ff4a5e40cd1a2d1bf6
atahan.celebi <atahan.celebi@synchron.com.tr>
2024-10-10 04:22:23 +0300

    azure-first-cloud-migrations

--- Migrations/20241010012013_InitialCreate-azure.Designer.cs
+++ Migrations/20241010012013_InitialCreate-azure.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductsAPI.Data;
 
@@ -11,9 +12,11 @@ using ProductsAPI.Data;
 namespace ProductsAPI.Migrations
 {
     [DbContext(typeof(MainDbContext))]
-    partial class MainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241010012013_InitialCreate-azure")]
+    partial class InitialCreateazure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
